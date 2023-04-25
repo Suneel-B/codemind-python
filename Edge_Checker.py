a1cf9d58778b25748e0181f5130e2b59f016fff4
Suneel-B <suneel@technicalhub.io>
2023-04-25 15:16:56 +0530

    a,b = list(map(int,input().split())) mini = min(a,b) maxi = max(a,b) if(maxi==10 and mini==1):     print("Yes") elif(mini+1 == maxi):     print("Yes") else:     print("No")

--- Edge_Checker.py
+++ Edge_Checker.py
@@ -2,8 +2,8 @@ a,b = list(map(int,input().split()))
 mini = min(a,b)
 maxi = max(a,b)
 if(maxi==10 and mini==1):
- print("Yes")
+    print("Yes")
 elif(mini+1 == maxi):
- print("Yes")
+    print("Yes")
 else:
- print("No")
+    print("No")
